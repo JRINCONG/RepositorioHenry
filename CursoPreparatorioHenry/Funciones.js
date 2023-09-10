f4cad20bc7d003eeceebdc2f702fcebdd536a83c
JRINCONG <rincon303@gmail.com>
2023-09-10 17:55:35 -0500

    Funciones

---
+++ CursoPreparatorioHenry/Funciones.js
@@ -0,0 +1,13 @@
+
+function SumaTres(x){
+    var r= x+5;
+    return r;
+
+}
+ 
+//Función: cuidadoConElConsoleLog
+function cuidadoConElConsoleLog(nombre) {
+    console.log(nombre);
+    return nombre;
+  }
+
