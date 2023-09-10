05d3956d18a1f5b172abe65dd47bc6b2d07d40e7
JRINCONG <rincon303@gmail.com>
2023-09-10 16:18:40 -0500

    Operadores JavaScript

---
+++ CursoPreparatorioHenry/Operadores.js
@@ -0,0 +1,14 @@
+//resta;
+console.log (5-3);
+// suma;
+console.log(25+89);
+//resto;
+console.log(45%6);
+//Multiplicacion;
+console.log(78*4);
+//Division
+console.log(45/3);
+//Precedencia por el usuario;
+console.log( (30+20)*(20-8));
+//Precedencia por Java
+console.log(4+5*4-7);
