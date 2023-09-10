7c8b02f2fa61125a2a9d64ae871ae05079c9ab95
JRINCONG <rincon303@gmail.com>
2023-09-10 16:45:25 -0500

    Operadores de Comprarcion

---
+++ CursoPreparatorioHenry/OperadoresDeComparacion.js
@@ -0,0 +1,17 @@
+console.log(5<9);
+console.log(30>20);
+console.log(20==30);
+
+
+//Igualda vs Igualdad stricta
+
+console.log(4=='4');
+console.log(5==='5');
+console.log(20===20);
+
+//Asignacion y Asociatividad
+
+var a=3;
+var b=7;
+var c= a=b;
+console.log(c);
