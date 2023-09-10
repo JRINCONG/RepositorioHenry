236701ca2d24f5aff8b307017172d2cb8918d858
JRINCONG <rincon303@gmail.com>
2023-09-10 18:30:05 -0500

    Condicionales

---
+++ CursoPreparatorioHenry/Condicionales.js
@@ -0,0 +1,9 @@
+function edad(x) {
+    if (x >= 18){
+        console.log('Eres Mayor de Edad');
+            } else if (x < 18){
+                console.log('Eres Menor de Edad');
+            } 
+
+}
+edad(18);
