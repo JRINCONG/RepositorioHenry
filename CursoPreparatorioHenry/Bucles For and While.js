634c9b1a967ee12012d1e6b6ad40545920973b21
JRINCONG <rincon303@gmail.com>
2023-09-11 12:53:43 -0500

    Bucles For and While

---
+++ CursoPreparatorioHenry/Bucles For and While.js
@@ -0,0 +1,21 @@
+function tabla(x){
+    var suma=0; 
+
+    for (var  i = x; i <=x; i++){
+         for  (var z=1; z<=10; z++){
+            
+            var suma =i * z;
+            console.log(x,'x',z,'=',suma);
+
+         }
+         console.log('La tabla del ',x);
+         console.log('terminado');
+
+    }
+}
+tabla(5);
+var suma=0;
+while(suma < 3) {
+    suma = suma + 1;
+        console.log(suma);
+}
