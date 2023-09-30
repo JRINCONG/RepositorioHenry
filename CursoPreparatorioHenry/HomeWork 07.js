63dab271c4ab6575bfc1e6e41c5f502f25b3e7fd
JRINCONG <rincon303@gmail.com>
2023-09-30 18:40:10 -0500

    Realizar ejercicios HomeWork 07

---
+++ CursoPreparatorioHenry/HomeWork 07.js
@@ -0,0 +1,49 @@
+// EJERCICIO 01:
+
+function crearUsuario() {
+    // Crea una Clase de ES6 o una función constructora llamada "Usuario".
+    // El constructor de esta clase debe tener las propiedades: "usuario", "nombre", "email" y "password".
+    // El valor de cada propiedad la recibirá por parámetro.
+    // Además, esta clase debe tener un método llamado "saludar".
+    // Este método debe retornar el string: 'Hola, mi nombre es {{nombre}}'.
+    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
+    // Retornar la clase.
+    // Tu código:
+ }
+ 
+ function agregarMetodoPrototype(Usuario) {
+    // Agrega un método al prototipo de "Usuario".
+    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
+    // Tu código:
+ }
+ 
+ function agregarStringInvertida() {
+    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
+    // El método debe llamarse "reverse".
+    // [PISTA]: necesitarás utilizar el objeto "this".
+ }
+
+
+ //EJERCICIO 02
+
+
+ class Persona {
+    // Crea el constructor de la clase "Persona".
+    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
+    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
+    // sus valores.
+ }
+ 
+ function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
+    // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
+    // Recibirás las propiedades por parámetro.
+    // Retornar la instancia creada.
+    // Tu código:
+ }
+ 
+ function agregarMetodo() {
+    // La función agrega un método "datos" a la clase "Persona".
+    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
+    // Ejemplo: "Juan, 22 años".
+    // Tu código:
+ }
