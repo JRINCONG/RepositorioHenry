8a07611010d728afe26eb3df667e5a806e6ea4e1
JRINCONG <rincon303@gmail.com>
2023-10-04 18:48:07 -0500

    Ejemplos de CallBack

---
+++ CursoPreparatorioHenry/Ejemplos de CallBack.js
@@ -0,0 +1,11 @@
+function devuelvoUsuario() {
+    return 'CAMILO';
+ }
+ function devuelvoSaludo() {
+    return 'Hola';
+ }
+ function saludar(cb1, cb2) {
+    return cb1() + ' ' + cb2();
+ }
+ var resultado = saludar(devuelvoSaludo, devuelvoUsuario);
+ console.log(resultado);
